59a8013c3d3a13a0599fbe30f48d97fada684eee
Hosam Azzam <hosam.azzam2@gmail.com>
2019-05-03 14:50:27 +0200

    uploaf to devinh

--- routes/console.php
+++ routes/console.php
@@ -15,4 +15,6 @@ use Illuminate\Foundation\Inspiring;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+    $this->comment(Inspiring::quote());
+    $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
