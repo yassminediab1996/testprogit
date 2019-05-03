dcebed0ecf6aef5755a94053b31470b4ed5ebea1
=yassmine <=yassmine@chefaa.com>
2019-05-03 15:05:47 +0200

    8ayert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,15 +29,6 @@
             margin: 0;
         }
 
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
         .full-height {
             height: 100vh;
         }
