1499a18f19db9f1f7849f3674c3fcf00a6b90ec9
=yassmine <=yassmine@chefaa.com>
2019-05-03 14:28:34 +0200

    update routes to be y

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', function () {
-    return view('welcome');
+    return view('y');
 });
 Route::get('/', function () {
     return view('welcome');
