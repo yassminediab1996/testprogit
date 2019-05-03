327d3e8fb0893c5413fb391f6af599784712357f
=yassmine <=yassmine@chefaa.com>
2019-05-03 14:48:17 +0200

    upllad to testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,24 @@
             margin: 0;
         }
 
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
         .full-height {
             height: 100vh;
         }
